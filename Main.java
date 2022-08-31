25f7790bd6ae42da17cbde8d49fa33c81916bcb6
C210040 <92075597+C210040@users.noreply.github.com>
2022-08-31 20:27:32 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("名前：");
+    
+    String firstName = sc.next();
+    
+    System.out.print("名前は" + firstName + "です");
+    
+  }
+}
